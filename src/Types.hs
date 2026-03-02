aa21576a11b14a496f089d2a03921f6ab4788646
saygo-png <saygo.mail@proton.me>
2026-03-02 23:47:38 +0100

    feat: remove useless types
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- src/Types.hs
+++ src/Types.hs
@@ -1,21 +1,8 @@
-module Types (ObjectTracker (..), BarState (..), WhichBuffer (..)) where
+module Types (BarState (..)) where
 
-import Data.Binary
-import Relude hiding (ByteString, get, isPrefixOf, length, put, replicate)
-import Saywayland.Types
+import Relude
 
 data BarState = BarState
   { date :: Text
   }
   deriving stock (Show)
-
-data WhichBuffer = BufferA | BufferB
-
-data ObjectTracker = ObjectTracker
-  { wl_surfaceID :: Maybe Word32
-  , wl_shm_poolID :: Maybe Word32
-  , wl_buffer_A :: Maybe Buffer
-  , wl_buffer_B :: Maybe Buffer
-  , zwlr_layer_surface_v1ID :: Maybe Word32
-  , zwlr_layer_surface_v1Serial :: TMVar Word32
-  }
